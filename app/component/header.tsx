83a804684036f50e6c7f2f3a6835dea12c176ac5
TheRobinLu <lu.robin@hotmail.com>
2023-09-12 21:38:53 -0400

    0.1.3 gradiant button, editPage layout

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<header className="flex items-center justify-between p-2 my-1 text-blue-100 bg-purple-400">
+		<header className="flex items-center justify-between p-1 my-1 text-blue-100 bg-purple-400">
 			<nav className="nav-bar flex gap-4 font-bold text-indigo-800 ">
 				<Link href="/">Home</Link>
 				<Link href="/edit">Edit</Link>
 				{/* <Link href="/about">About</Link> */}
 			</nav>
-			<div className="flex gap-2 right-0 text-xs font-light text-indigo-600">
-				ver: 0.1.2
+			<div className="flex gap-2 right-0 text-xs font-light text-indigo-800">
+				Version: 0.1.3
 			</div>
 		</header>
 	);
